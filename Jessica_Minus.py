70efafe6f62565327fe7c418ddbe022b2511f83a
Jessica <jessicaoctaviani@gmail.com>
2025-09-24 16:50:47 +0800

    Create two files

---
+++ Jessica_Minus.py
@@ -0,0 +1,15 @@
+#
+# Jessica, 2025/09/24
+# File: Jessica_Minus.py
+# Short description of the task
+#
+
+# 1. Input
+X = 8
+Y = 7
+
+# 2. Process
+Sum = X - Y
+
+# 3. Output
+print(f'Result {Sum}')
--- Jessica_sum.py
+++ Jessica_sum.py
@@ -1,11 +1,15 @@
 #
 # Jessica, 2025/9/24
 # File: Jessica_Sum.py
-# Short description of the task
+# Add two numbers then print
 #
 
 # 1. Input
+X = 8
+Y = 7
 
 # 2. Process
+Sum = X + Y
 
 # 3. Output
+print(f'Result {Sum}')
